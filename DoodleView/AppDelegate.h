7c25c7cb8d35d9074e5ddbb5499332c8788226b4
Lanou <lanou@LanoutekiMac-mini-4.local>
2014-01-21 10:27:21 +0800

    Initial Commit

---
+++ DoodleView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DoodleView
+//
+//  Created by Lanou on 14-1-21.
+//  Copyright (c) 2014年 Lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DoodleView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DoodleView
+//
+//  Created by Lanou on 14-1-21.
+//  Copyright (c) 2014年 Lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
